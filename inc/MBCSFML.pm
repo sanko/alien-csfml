83795511fe3612f5349bc6352b3fc66788c05cf0
Sanko Robinson <sanko@cpan.org>
2022-10-21 08:24:16 -0400

    Check exit value of system call to figure out what's going on with #1.

--- inc/MBCSFML.pm
+++ inc/MBCSFML.pm
@@ -101,8 +101,8 @@ sub build_libs {
     my $cwd  = rel2abs './';       # XXX - use Cwd;
 
     # This is an ugly cludge. A working, ugly cludge though. :\
-    if ( !-d 'share' ) {
-        mkpath( 'share', $options->{verbose}, oct '755' ) unless -d 'share';
+    if ( !-d catdir( $cwd, 'share' ) ) {
+        mkpath( catdir( $cwd, 'share' ), $options->{verbose}, oct '755' ) unless -d 'share';
         $dir = tempd();
         my $archdir = catdir( $cwd, qw[share] );
         if ( $^O eq 'MSWin32' ) {
@@ -150,22 +150,37 @@ sub build_libs {
         my $exe = Alien::cmake3->exe;
         my $win = $^O eq 'MSWin32' ? 1 : 0;
         my $mac = $^O eq 'darwin'  ? 1 : 0;
-        $exe = qq["$exe" -G"MinGW Makefiles"] if $win;
+        $exe = qq["$exe" -G"MinGW Makefiles"] if $win;    # uses prebuilt libs but don't remove yet
 
         #$exe = qq[sudo $exe]                  if $mac;
-        warn($_) && system($_)
-            for $exe .
+        for my $cmd (
+            $exe .
             " -S $cwd/cmake/sfml -B ./build/sfml -DCMAKE_INSTALL_PREFIX=$cwd/share/sfml -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=TRUE -DSFML_BUILD_EXAMPLES=FALSE -DSFML_BUILD_TEST_SUITE=FALSE"
-            . ( $mac ? ' -DSFML_BUILD_FRAMEWORKS=FALSE' : '' ),
-            ( $win   ? ( 'mingw32-make -C ./build/sfml', 'mingw32-make -C ./build/sfml install' ) :
-                $exe . " --build ./build/sfml --config Release --parallel 5 --target install" ),
+            . ( $mac ? ' -DSFML_BUILD_FRAMEWORKS=FALSE' : '' ), (
+                $win ? ( 'mingw32-make -C ./build/sfml', 'mingw32-make -C ./build/sfml install' ) :
+                    $exe . " --build ./build/sfml --config Release --parallel 5 --target install"
+            ),
             $exe .
             " -S $cwd/cmake/csfml -B ./build/csfml -DCMAKE_INSTALL_PREFIX=$cwd/share/csfml -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=TRUE -DCSFML_LINK_SFML_STATICALLY=FALSE -DSFML_DIR=$cwd/share/sfml/lib/cmake/SFML/"
-            . ( $mac ? ' -DSFML_BUILD_FRAMEWORKS=FALSE' : '' ),
-            ( $win ? ( 'mingw32-make -C ./build/csfml', 'mingw32-make -C ./build/csfml install' ) :
-                $exe . " --build ./build/csfml --config Release --parallel 5 --target install" );
-
-        #write_file( catfile( $archdir, qw[config.json] ), 'utf8', encode_json( \%libinfo ) );
+            . ( $mac ? ' -DSFML_BUILD_FRAMEWORKS=FALSE' : '' ), (
+                $win ?
+                    ( 'mingw32-make -C ./build/csfml', 'mingw32-make -C ./build/csfml install' ) :
+                    $exe . " --build ./build/csfml --config Release --parallel 5 --target install"
+            )
+        ) {
+            print "# $cmd\n";
+            system $cmd;
+            if ( $? == -1 ) {
+                die "# failed to execute: $!\n";
+            }
+            elsif ( $? & 127 ) {
+                die sprintf "# child died with signal %d, %s coredump\n", ( $? & 127 ),
+                    ( $? & 128 ) ? 'with' : 'without';
+            }
+            else {
+                printf "# child exited with value %d\n", $? >> 8;
+            }
+        }
     }
 }
 
